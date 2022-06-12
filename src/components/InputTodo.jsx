4fa6db9fae37e3dffa17e0aa0fcc2436871eb64a
Sota <30281161+sota0121@users.noreply.github.com>
2022-06-12 15:34:42 +0900

    use CSS-in-JS for input-area class

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import "../styles.css";
 
+const style = {
+  backgroundColor: "#cfffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 export const InputTodo = (props) => {
   const { onClick, onChange, inputText } = props;
 
   return (
-    <div className="input-area">
+    <div style={style} className="input-area">
       <input
         id="add-text"
         placeholder="Input ToDo"
